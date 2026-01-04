aa17fed487cd4eae6da72c591a1cd172372220c8
Otman Ouali <otto.ouali@gmail.com>
2026-01-04 21:01:57 +1000

    Restructuring the repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ottowoolf.github.io',
-  base: '/OPortfolio', // This adds the prefix to all generated links
+  site: 'https://ottowoolf.github.io'
 });
